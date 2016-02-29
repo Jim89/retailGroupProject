848e08a447b2044541cd3be2897643b51a37f679
Jim Leach <jimmy22theave@gmail.com>
2016-02-29 08:59:45 +0000

    Create further numerical summaries, and add further exploratory plots

--- jim/01_explore.R
+++ jim/01_explore.R
@@ -17,12 +17,18 @@ complete_rows <- complete.cases(coffee) %>% sum
 cols <- ncol(coffee)
 complete_cols <- apply(apply(coffee, 2, complete.cases), 2, sum)
 
+# Convert certain fields to factor
+# coffee <- coffee %>% 
+#           mutate(year = as.factor(year),
+#                  day = as.factor(day),
+#                  packs = as.factor(packs))
+                 
 
-# Step 2 - Create some plots of rows by values ---------------------------------
+# Step 2 - Create some exploratory plots ---------------------------------
 # Create function to draw histogram
 gg_hist <- function(data, field){
   is_num <- is.numeric(data[ , field])
-  name <- paste0("./jim/images/", field, ".svg")
+  name <- paste0("./jim/images/fields/", field, ".svg")
   if (is_num == TRUE) {
     plot <- ggplot(data, aes_string(x = field)) +
       geom_histogram(fill = "steelblue", colour = "white") +
@@ -41,6 +47,29 @@ gg_hist <- function(data, field){
 columns <- colnames(coffee)
 lapply(columns, function(x) gg_hist(coffee, x))
 
-pairs <- ggpairs(coffee
+# Examine total sales over time
+total_spend <- coffee %>% 
+                group_by(relweek) %>% 
+                summarise(total_spend = sum(netspend, na.rm = TRUE)) %>% 
+                ggplot(aes(x = relweek, y = total_spend)) +
+                geom_line(color = "steelblue") + 
+                theme_minimal()
+ggsave("./jim/images/fieldRelationships/spendVsWeek.svg", total_spend, 
+       height = 8, width = 10)
+
+
+# Examine sales over time by shop
+total_spend_by_shop <- coffee %>% 
+                        group_by(shop_desc,
+                                 relweek) %>% 
+                        summarise(total_spend = sum(netspend, na.rm = TRUE)) %>%
+                        ggplot(aes(x = relweek, y = total_spend, colour = shop_desc)) +
+                        geom_line(aes(colour = shop_desc), # alpha = .75,
+                                  size = .75) +
+                        theme_minimal()
+ggsave("./jim/images/fieldRelationships/spendVsWeekByShop.svg", total_spend_by_shop,
+       height = 10, width = 12)
+
+
 
 

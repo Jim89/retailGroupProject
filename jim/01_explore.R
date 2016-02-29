fa17620a377d91743c0467fc33fe0dfafbbb54a6
Jim Leach <jimmy22theave@gmail.com>
2016-02-29 08:01:57 +0000

    Update naming to "coffee", change function to reflect project structure

--- jim/01_explore.R
+++ jim/01_explore.R
@@ -3,26 +3,26 @@ library(dplyr)
 library(ggplot2)
 library(GGally)
 
-# Step 1 - read data -----------------------------------------------------------
-data <- read.csv("./data/InstantCoffee.csv", 
+# Step 1 - read coffee -----------------------------------------------------------
+coffee <- read.csv("./data/InstantCoffee.csv", 
                    encoding = "latin1", 
                    stringsAsFactors = FALSE) %>% 
           as_data_frame()
 
-colnames(data) <- tolower(colnames(data))
+colnames(coffee) <- coffee %>% colnames() %>% tolower()
 
-rows <- nrow(data)
-complete_rows <- complete.cases(data) %>% sum
+rows <- nrow(coffee)
+complete_rows <- complete.cases(coffee) %>% sum
 
-cols <- ncol(data)
-complete_cols <- apply(apply(data, 2, complete.cases), 2, sum)
+cols <- ncol(coffee)
+complete_cols <- apply(apply(coffee, 2, complete.cases), 2, sum)
 
 
 # Step 2 - Create some plots of rows by values ---------------------------------
 # Create function to draw histogram
 gg_hist <- function(data, field){
   is_num <- is.numeric(data[ , field])
-  name <- paste0("./explorations/images/", field, ".svg")
+  name <- paste0("./jim/images/", field, ".svg")
   if (is_num == TRUE) {
     plot <- ggplot(data, aes_string(x = field)) +
       geom_histogram(fill = "steelblue", colour = "white") +
@@ -38,7 +38,9 @@ gg_hist <- function(data, field){
   ggsave(file = name, plot = plot, width = 10, height = 8)
 }  
 
-columns <- colnames(data)
-lapply(columns, function(x) gg_hist(data, x))
+columns <- colnames(coffee)
+lapply(columns, function(x) gg_hist(coffee, x))
+
+pairs <- ggpairs(coffee
 
 

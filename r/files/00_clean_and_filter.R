602645a44f75eaba371b8edf602a3e2f0e597c56
Jim Leach <jimmy22theave@gmail.com>
2016-03-01 16:43:15 +0000

    Create cleaning and filtering script

---
+++ r/files/00_clean_and_filter.R
@@ -0,0 +1,31 @@
+# Step 0 - load packages -------------------------------------------------------
+library(dplyr)
+
+
+# Step 1 - read coffee ----------------------------------------------------------
+coffee <- read.csv("./data/InstantCoffee.csv", 
+                   encoding = "latin1", 
+                   stringsAsFactors = FALSE) %>% 
+          as_data_frame()
+
+colnames(coffee) <- coffee %>% colnames() %>% tolower()
+
+# Step 2 - filter and clean ----------------------------------------------------
+coffee_clean <- coffee %>% # Take coffee data then
+                # Filter it
+                filter(shop_desc %in% c("1TESCO", # Only take certain shops
+                                        "2ASDA", 
+                                        "3SAINSBURYS", 
+                                        "4MORRISONS", 
+                                        "7DICSOUNTERS Aldi", 
+                                        "7DISCOUNTERS Lidl")
+                       & # And
+                       sub_cat_name %in% c("Granules", # Only take certain coffees
+                                           "Freeze Dried",
+                                           "Decaf Freeze Drief", 
+                                           "Micro Ground")) %>% # then...
+                mutate(shop_desc_clean = shop_desc %>% # add cleaner shop name
+                                          substring(2) %>% 
+                                          tolower() %>% 
+                                          gsub("discounters ","", .))
+

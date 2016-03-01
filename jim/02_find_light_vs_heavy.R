27a58140a34943a92cceb6473aa58053b3c34ae4
Jim Leach <jimmy22theave@gmail.com>
2016-03-01 17:26:26 +0000

    Add function definition to script

--- jim/02_find_light_vs_heavy.R
+++ jim/02_find_light_vs_heavy.R
@@ -4,6 +4,18 @@ library(magrittr)
 
 source("./r/analysis.R")
 
+summarise_by_fields <- function(data, field1, field2) {
+  data %>% 
+    group_by_(field1, field2) %>% 
+    summarise(records = n(),
+              total_packs = sum(packs),
+              avg_packs = mean(packs),
+              sd_packs = sd(packs),
+              total_spend = sum(netspend),
+              avg_spend = mean(netspend),
+              sd_spend = sd(netspend))
+}  
+
 
 # Step 1 - find light vs heavy users -------------------------------------------
 houseByWeek <- summarise_by_fields(coffee_clean, "relweek", "house")
@@ -15,6 +27,6 @@ house_summary <- houseByWeek %>%
                         avg_weekly_spend = mean(total_spend))
 
 house_summary %<>% 
-  mutate(heavy = ifelse(avg_weekly_visits >= percentile(house_summary$avg_weekly_spend)[2], 1, 0))
+  mutate(heavy = ifelse(avg_weekly_visits >= quantile(house_summary$avg_weekly_spend)[2], 1, 0))
 
 sum(house_summary$heavy)/nrow(house_summary)

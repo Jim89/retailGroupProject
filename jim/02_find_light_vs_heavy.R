aa45418e4373e817ac16dd2cbd2b30eca3f68b8a
Jim Leach <jimmy22theave@gmail.com>
2016-03-02 10:18:16 +0000

    Convert avg weekly rent to z-score to split heavy/light

--- jim/02_find_light_vs_heavy.R
+++ jim/02_find_light_vs_heavy.R
@@ -16,6 +16,13 @@ summarise_by_fields <- function(data, field1, field2) {
               sd_spend = sd(netspend))
 }  
 
+normalise <- function(x) {
+  avg <- mean(x)
+  std <- sd(x)
+  ans <- (x-avg)/std
+  return(ans)
+}  
+
 
 # Step 1 - find light vs heavy users -------------------------------------------
 houseByWeek <- summarise_by_fields(coffee_clean, "relweek", "house")
@@ -24,9 +31,13 @@ house_summary <- houseByWeek %>%
               group_by(house) %>% 
               summarise(avg_weekly_visits = mean(records),
                         avg_weekly_packs = mean(total_packs),
-                        avg_weekly_spend = mean(total_spend))
+                        avg_weekly_spend = mean(total_spend)) %>% 
+              mutate(avg_weekly_spend_norm = normalise(avg_weekly_spend))
 
 house_summary %<>% 
-  mutate(heavy = ifelse(avg_weekly_visits >= quantile(house_summary$avg_weekly_spend)[2], 1, 0))
+  mutate(heavy = ifelse(avg_weekly_visits >= quantile(house_summary$avg_weekly_spend,
+                                                      probs = seq(0, 1, 0.1))[2], 1, 0),
+         heavy2 = ifelse(avg_weekly_spend_norm > 0, 1, 0))
 
 sum(house_summary$heavy)/nrow(house_summary)
+sum(house_summary$heavy2)/nrow(house_summary)

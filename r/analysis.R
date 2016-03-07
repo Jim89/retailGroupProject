1243027893e5ea840ccf02bee247f4c94161ed53
Jim Leach <jimmy22theave@gmail.com>
2016-03-07 09:48:36 +0000

    Add new file to analysis script and source function definition

--- r/analysis.R
+++ r/analysis.R
@@ -2,8 +2,10 @@
 library(dplyr)
 
 # Step 1 - source user-defined functions----------------------------------------
+source("./r/functions/normalise.R")
 
 
 # Step 2 - run scripts in order ------------------------------------------------
 source("./r/files/00_clean_and_filter.R")
 source("./r/files/01_light_vs_heavy.R")
+source("./r/files/02_clean_brands.R")

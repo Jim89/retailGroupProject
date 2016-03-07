d5b3bc35519f2316e47985a03f3573ca03058620
Jim Leach <jimmy22theave@gmail.com>
2016-03-07 10:13:46 +0000

    Add code to aggregate data to weekly level ahead of widening

--- jim/05_widen.R
+++ jim/05_widen.R
@@ -1,2 +1,29 @@
 # Step 0 - prepare environment -------------------------------------------------
 source("./r/analysis.R")
+
+
+# Step 1 - widen
+library(tidyr)
+
+# Agggregate
+coffee_aggregated <- coffee_clean %>% 
+  group_by(relweek, heavy, brand_clean) %>% 
+  summarise(packs = sum(packs),
+            avg_price = mean(price))
+
+coffee_aggregated %>% 
+  filter(heavy == 0) %>% 
+  select(-heavy, - packs) %>% 
+  spread(brand_clean, avg_price) %>% View
+
+filter_and_summarise <- function(data, cust_status = 0) {
+  data %>% 
+    filter(heavy == cust_status) %>% 
+    group_by(relweek, brand_clean) %>% 
+    summarise(sales = sum(packs),
+              price = mean(price),
+              promo_sales = sum(promo)) %>% 
+    mutate(prop_promo = promo_sales / sales)
+}
+
+heavy <- filter_and_summarise(coffee_clean, cust_status = 1)

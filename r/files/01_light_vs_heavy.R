c432b7cdb92302bd84554451081d43dcaf69e35a
Jim Leach <jimmy22theave@gmail.com>
2016-03-09 07:35:50 +0000

    Split light vs. heavy by bottom 25% (light) and top 25% (heavy) of houses based on weekly volume of coffee bought

--- r/files/01_light_vs_heavy.R
+++ r/files/01_light_vs_heavy.R
@@ -10,20 +10,22 @@ house_summary <- coffee_clean %>%
                           sd_packs = sd(packs),
                           total_spend = sum(netspend),
                           avg_spend = mean(netspend),
-                          sd_spend = sd(netspend)) %>% 
+                          sd_spend = sd(netspend),
+                          total_vol = sum(volume)) %>% 
                 ungroup() %>% 
                 group_by(house) %>% 
                 summarise(avg_weekly_visits = mean(records),
                           avg_weekly_packs = mean(total_packs),
-                          avg_weekly_spend = mean(total_spend)) %>% 
-                mutate(avg_weekly_spend_norm = normalise(avg_weekly_spend))
+                          avg_weekly_spend = mean(total_spend),
+                          avg_weekly_vol = mean(total_vol)) 
 
 # Step 2 - classify into light vs heavy ----------------------------------------
+quartiles <- quantile(house_summary$avg_weekly_vol)
 light_vs_heavy <- house_summary %>% 
-                  mutate(heavy = ifelse(avg_weekly_spend >= 
-                                          median(house_summary$avg_weekly_spend)
-                                                 , 1, 0)) %>% 
-                  select(house, heavy)
+                  mutate(cust_type = ifelse(avg_weekly_vol <= quartiles[2], "light",
+                                      ifelse(avg_weekly_vol >= quartiles[4], "heavy", 
+                                             "medium"))) %>% 
+                  select(house, cust_type)
 
 # Step 3 - perform the join ----------------------------------------------------
 coffee_clean <- coffee_clean %>% left_join(light_vs_heavy, by = "house")

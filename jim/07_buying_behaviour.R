c47974bf5da3d62edeeecf9ab9a7facd6932c8f1
Jim Leach <jimmy22theave@gmail.com>
2016-03-14 12:26:51 +0000

    Clean up code and add comments; fit random forest algorithm to the data to assess importance of variables in splitting heavy vs. light; and create some visualisations of variable importance and splits by heavy/light for certain fields

--- jim/07_buying_behaviour.R
+++ jim/07_buying_behaviour.R
@@ -1,3 +1,4 @@
+# Step 0 - set up environment --------------------------------------------------
 library(ggbiplot)
 library(dplyr)
 library(ggplot2)
@@ -8,6 +9,7 @@ source("./r/files/00_clean_and_filter.R")
 source("./r/files/01_light_vs_heavy.R")
 source("./r/files/02_clean_brands.R")
 
+# Step 1 - create house level summary stats ------------------------------------
 house_summary <- coffee_clean %>% 
                   group_by(relweek, house) %>% 
                   summarise(records = n(),
@@ -54,7 +56,7 @@ promo_stats <- coffee_clean %>%
                        #prop_promo_none = 1 - prop_promo_price - prop_promo_units) %>% 
                 select(-promo_price, -promo_units, -total_purchases)
 
-# Combine
+# Combine in to single data set
 quartiles <- quantile(house_summary$avg_weekly_vol)
 houses <- house_summary %>% 
           left_join(distinct_shops) %>% 
@@ -64,23 +66,42 @@ houses <- house_summary %>%
           mutate(cust_type = ifelse(avg_weekly_vol <= quartiles[2], "light",
                                     ifelse(avg_weekly_vol >= quartiles[4], "heavy", 
                                            "medium"))) %>% 
-          filter(cust_type != "medium")
+          filter(cust_type != "medium") 
 
 
-# kmeans
-kfit <- kmeans(houses[, -c(1, 13)], 2, iter.max = 200)
+# Step 2 - run k-means over the data to see if clusters can be separated -------
+kfit <- kmeans(houses %>% select(-house, -cust_type, -avg_weekly_vol),
+               2, iter.max = 200)
+
 houses$cluster <- kfit$cluster
 
+# Check accuracy of k-means wrt actual labels
+diag_sum <- table(houses$cluster, houses$cust_type) %>% as.matrix() %>% diag %>% sum
+accuracy <- diag_sum/nrow(houses)
 
-houses_pca <- prcomp(houses %>% select(-house, -cust_type, -cluster),
+
+# Step 3 - Perform PCA to help visualise the whole data ------------------------
+houses_pca <- prcomp(houses %>% select(-house, -cust_type, -cluster, -avg_weekly_vol),
                      center = TRUE,
                      scale = TRUE)
 
+# Step 4 - Perform random forest prediction for variable importance ------------
+library(randomForest)
+
+rf <- randomForest(as.factor(cust_type) ~ ., 
+                   data = houses %>% select(-house, -cluster, -avg_weekly_vol),
+                   importance = TRUE)
+
+varImpPlot(rf, 
+           main = "Variable importance for determining heavy vs. light users",
+           type = 1)
 
 
+# Step 4 - Visualise! ----------------------------------------------------------
+# Set up common theme template
 theme <- theme(legend.position = "bottom",
            axis.text.y = element_text(size = 16, colour = "black"),
-           axis.text.x = element_text(size = 16, colour = "black", angle = -90),
+           axis.text.x = element_text(size = 16, colour = "black"),
            legend.text = element_text(size = 16),
            legend.title = element_text(size = 16),
            title = element_text(size = 16),
@@ -92,21 +113,44 @@ theme <- theme(legend.position = "bottom",
            panel.background = element_rect(fill = "white", colour = "lightgrey"),
            panel.border = element_rect(colour = "black", fill = NA))
 
+## PCA plot coloured by k-means clusters
+k_means_pcs <- ggbiplot(houses_pca, obs.scale = 1, var.scale = 1, 
+                         groups = as.factor(houses$cluster), ellipse = TRUE, 
+                         circle = F, alpha = 0.25, var.axes = F, 
+                         size = 2.5) +
+                scale_color_brewer(type = "qual", palette = "Dark2") + 
+                guides(colour = guide_legend(title = "Customer Type")) +
+                theme
 
-ggbiplot(houses_pca, obs.scale = 1, var.scale = 1, 
-         groups = as.factor(houses$cluster), ellipse = TRUE, 
-         circle = F, alpha = 0.25, var.axes = F, 
-         size = 2.5) +
-  scale_color_brewer(type = "qual", palette = "Dark2") + 
-  guides(colour = guide_legend(title = "Customer Type")) +
-  theme
+# PCA plot coloured by customer type
+cust_type_pca <- ggbiplot(houses_pca, obs.scale = 1, var.scale = 1, 
+                           groups = houses$cust_type, ellipse = TRUE, 
+                           circle = F, alpha = 0.25, var.axes = F, 
+                           size = 2.5) +
+                  scale_color_brewer(type = "qual", palette = "Dark2") + 
+                  guides(colour = guide_legend(title = "Customer Type")) +
+                  theme
+
+# Function to visualise field split by light vs heavy 
+dist_split <- function(data, field) {
+  data %>% 
+    ggplot(aes_string(x = field)) +
+    geom_histogram(aes(fill = cust_type), colour = "white") +
+    scale_fill_brewer(type = "qual", palette = "Dark2") + 
+    guides(fill = guide_legend(title = "Customer Type")) +
+    facet_grid(. ~ cust_type) +
+    ylab("Count") +
+    theme
+}
+
+# Split plot by prop promo price
+dist_split(houses, "prop_promo_price") + xlab("Proportion of goods bought on price promotion")
+
+# Split plot by max spend
+dist_split(houses, "max_spend") + xlab("Maximum spend in any single trip")
+
+# Split plot by avg_weekly_spend
+dist_split(houses, "avg_weekly_spend") + xlab("Average weekly spend")
 
 
-ggbiplot(houses_pca, obs.scale = 1, var.scale = 1, 
-         groups = houses$cust_type, ellipse = TRUE, 
-         circle = F, alpha = 0.25, var.axes = F, 
-         size = 2.5) +
-  scale_color_brewer(type = "qual", palette = "Dark2") + 
-  guides(colour = guide_legend(title = "Customer Type")) +
-  theme
 

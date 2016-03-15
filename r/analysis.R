e3930c05bc02b82704f1f4bce3d90fa5146ac292
Jim Leach <jimmy22theave@gmail.com>
2016-03-15 16:30:48 +0000

    Incorporate visualisation code to analysis, and write plots to file

--- r/analysis.R
+++ r/analysis.R
@@ -10,6 +10,22 @@ library(ggplot2)
 source("./r/functions/normalise.R")
 source("./r/functions/toproper.R")
 
+
+# Step 2 - run scripts in order ------------------------------------------------
+# Run Analysis code
+source("./r/files/00_clean_and_filter.R")
+source("./r/files/01_light_vs_heavy.R")
+source("./r/files/02_clean_brands.R")
+source("./r/files/03_prepare_for_modelling.R")
+source("./r/files/04_heavy_elast.R")
+source("./r/files/05_light_elast.R")
+source("./r/files/06_clout_and_vuln_stats.R")
+source("./r/files/07_write_elasticity_results_to_file.R")
+source("./r/files/08_buying_behaviour_data.R")
+source("./r/files/09_buying_behaviour_classify.R")
+
+
+# Step 3 - run visualisation scripts in order ----------------------------------
 # Set up custom theme to be applied to all plot objects
 theme <- theme(legend.position = "bottom",
                axis.text.y = element_text(size = 16, colour = "black"),
@@ -27,20 +43,25 @@ theme <- theme(legend.position = "bottom",
                panel.background = element_rect(fill = "white", colour = "lightgrey"),
                panel.border = element_rect(colour = "black", fill = NA))
 
-
-# Step 2 - run scripts in order ------------------------------------------------
-# Run Analysis code
-source("./r/files/00_clean_and_filter.R")
-source("./r/files/01_light_vs_heavy.R")
-source("./r/files/02_clean_brands.R")
-source("./r/files/03_prepare_for_modelling.R")
-source("./r/files/04_heavy_elast.R")
-source("./r/files/05_light_elast.R")
-source("./r/files/06_clout_and_vuln_stats.R")
-source("./r/files/07_write_elasticity_results_to_file.R")
-source("./r/files/08_buying_behaviour_data.R")
-source("./r/files/09_buying_behaviour_classify.R")
-
 # Run plotting code
 source("./r/files/10_clout_and_vuln_map.R")
 
+source("./r/files/12_buying_behaviour_charts.R")
+
+
+# Step 3 - write plots to file -------------------------------------------------
+if (!dir.exists("./visualisations")) {
+  dir.create("./visualisations")
+}
+# Save clout and vulnerability map
+ggsave("./visualisations/clout_and_vuln_map.svg", clout_and_vuln_map)
+
+# Save variable importance plot
+
+# Find all buying behaviour plots in the workspace (all prefaced with "bb_")
+bbplots <- ls()[grep("bb_", ls())] 
+
+# Loop over buy-behav plots and write to SVG files
+res <- lapply(bbplots, function(x) ggsave(paste0("./visualisations/", x,".svg"),
+                                   eval(parse(text = x))))
+rm(res)

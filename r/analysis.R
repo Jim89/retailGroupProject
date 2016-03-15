58b598d00d1c14886c7f1df630d62d4973b006bc
Jim Leach <jimmy22theave@gmail.com>
2016-03-15 15:48:56 +0000

    Include clout and vulnerability map in build

--- r/analysis.R
+++ r/analysis.R
@@ -8,9 +8,28 @@ library(ggplot2)
 
 # Step 1 - source user-defined functions----------------------------------------
 source("./r/functions/normalise.R")
+source("./r/functions/toproper.R")
+
+# Set up custom theme to be applied to all plot objects
+theme <- theme(legend.position = "bottom",
+               axis.text.y = element_text(size = 16, colour = "black"),
+               axis.text.x = element_text(size = 16, colour = "black"),
+               legend.text = element_text(size = 16),
+               legend.title = element_text(size = 16),
+               title = element_text(size = 16),
+               strip.text = element_text(size = 16, colour = "black"),
+               strip.background = element_rect(fill = "white"),
+               panel.grid.minor.x = element_blank(),
+               panel.grid.major.x = element_line(colour = "grey", linetype = "dotted"),
+               panel.grid.minor.y = element_line(colour = "lightgrey", linetype = "dotted"),
+               panel.grid.major.y = element_line(colour = "grey", linetype = "dotted"),
+               panel.margin.y = unit(0.1, units = "in"),
+               panel.background = element_rect(fill = "white", colour = "lightgrey"),
+               panel.border = element_rect(colour = "black", fill = NA))
 
 
 # Step 2 - run scripts in order ------------------------------------------------
+# Run Analysis code
 source("./r/files/00_clean_and_filter.R")
 source("./r/files/01_light_vs_heavy.R")
 source("./r/files/02_clean_brands.R")
@@ -21,4 +40,7 @@ source("./r/files/06_clout_and_vuln_stats.R")
 source("./r/files/07_write_elasticity_results_to_file.R")
 source("./r/files/08_buying_behaviour_data.R")
 source("./r/files/09_buying_behaviour_classify.R")
+
+# Run plotting code
+source("./r/files/10_clout_and_vuln_map.R")
 
---
+++ r/files/10_clout_and_vuln_map.R
@@ -0,0 +1,19 @@
+# Step 0 - load packages -------------------------------------------------------
+
+# Step 1 - Create base plot ----------------------------------------------------
+clout_and_vuln_map <- clout_and_vuln_stats %>%
+                      mutate(brand = toproper(brand),
+                             cust = toproper(cust)) %>% 
+                      ggplot(aes(x = vuln, y = clout)) +
+                      geom_point(size = 7.5, aes(colour = brand)) +
+                      scale_color_brewer(type = "qual", palette = "Dark2") + 
+                      facet_grid( . ~ cust) +
+                      xlab("Vulnerability") +
+                      ylab("Clout") +
+                      guides(colour = guide_legend(title = "Brand")) +
+                      geom_hline(yintercept = 0,
+                                 colour = "black", linetype = "dotted") +
+                      geom_vline(xintercept = 0,
+                                 colour = "black", linetype = "dotted") +
+                      theme
+

2897298bd0bd52e3e676a7c1a71ad5899cd7d414
Jim Leach <jimmy22theave@gmail.com>
2016-03-16 10:08:47 +0000

    Generate cooccurence for light and heavy users separately

--- jim/09_multibrand_buys.R
+++ jim/09_multibrand_buys.R
@@ -12,16 +12,24 @@ trans_id <- coffee_clean %>%
 coffee_clean <- coffee_clean %>% left_join(trans_id)
 
 # Find unique brands for each transaction
-brands_per_id <- coffee_clean %>% 
-                  select(transaction_id, brand_clean) %>% 
-                  distinct()
+brands_per_id_light <- coffee_clean %>% 
+                        filter(cust_type == "light") %>% 
+                        select(transaction_id, brand_clean) %>% 
+                        distinct()
+
+brands_per_id_heavy <- coffee_clean %>% 
+                        filter(cust_type == "heavy") %>% 
+                        select(transaction_id, brand_clean) %>% 
+                        distinct()
 
 # Step 2 - reshape to co-occurence ---------------------------------------------
 # Get co-occurenc matrix (may be inefficient on large data)
-cooccurence <- brands_per_id %>% table() %>% crossprod()
+cooccurence_light <- brands_per_id_light %>% table() %>% crossprod()
+cooccurence_heavy <- brands_per_id_heavy %>% table() %>% crossprod()
 
 # Set diagonal to 0
-diag(cooccurence) <- 0
+diag(cooccurence_light) <- 0
+diag(cooccurence_heavy) <- 0
 
 
 

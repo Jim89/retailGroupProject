2d372379487f78583271332603f819215b2e9313
Jim Leach <jimmy22theave@gmail.com>
2016-03-16 10:30:37 +0000

    Add visualisation of proportion of brands purchased by cust type

--- jim/10_brands_purchased.R
+++ jim/10_brands_purchased.R
@@ -6,3 +6,24 @@ brands_purchased <- coffee_clean %>%
                     group_by(cust_type, brand_clean) %>% 
                     tally() %>% 
                     spread(cust_type, n)
+
+brands_purchased <- brands_purchased %>% 
+                    mutate(heavy_prop = heavy / sum(brands_purchased$heavy),
+                           light_prop = light / sum(brands_purchased$light))
+
+# Step 2 - visualise -----------------------------------------------------------
+brands_purchased %>% 
+  select(-heavy, -light) %>% 
+  separate(brand_clean, into = c("brand_clean", "null"), sep = " ") %>% 
+  select(-null) %>% 
+  gather(cust, purchases, -brand_clean) %>% 
+  mutate(cust = ifelse(cust == "heavy_prop", "Heavy", "Light")) %>% 
+  ggplot(aes(x = brand_clean, y = purchases)) +
+  geom_bar(stat = "identity", aes(fill = cust)) +
+  scale_fill_brewer(type = "qual", palette = "Dark2") + 
+  guides(fill = guide_legend(title = "Customer Type")) +
+  facet_grid(cust ~ .) +
+  xlab("Brand") +
+  ylab("Proportion of purchases") +
+  theme +
+  theme(strip.text = element_blank())

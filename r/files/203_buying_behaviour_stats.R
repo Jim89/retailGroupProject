d0be16cfb5bb61179da83bfd804f08102a9d82b2
Jim Leach <jimmy22theave@gmail.com>
2016-03-18 15:01:46 +0000

    Extend buying behaviour stats to all fields

--- r/files/203_buying_behaviour_stats.R
+++ r/files/203_buying_behaviour_stats.R
@@ -1,96 +1,161 @@
-# Step 1 - load packages -------------------------------------------------------
+# Step 0 - prep env ------------------------------------------------------------
+# Function to compute variances
+get_var <- function(field, custs) {
+  buying_behaviour %>% 
+    filter(cust_type == custs) %>% 
+    select_(field) %>% 
+    var()
+}
 
 
-# Step 2 - analyze differences in average weekly spend/visits/packs/vol in turn 
+# Function to perform variance test
+var_test <- function(field) {
+  var_heavy <- buying_behaviour %>% 
+                filter(cust_type == "Heavy") %>% 
+                select_(field) %>% sapply(as.numeric)
+  
+  var_light <- buying_behaviour %>% 
+                filter(cust_type == "Light") %>% 
+                select_(field) %>% sapply(as.numeric)
+  
+  var_test <- var.test(var_heavy, var_light, alternative = "two.sided") %>% tidy()
+  
+  return(var_test)
+}
 
-#    The approach is as follows:
-# 1. Inspecting variances and formally testing for equality using F-test
-# 2. Depending on outcome of F-test testing for significant differences in means
-#    using the independent two-samples t-test (equal or unequal variances)
-# 3. Testing for equality of the distributions using Wilcoxon-Mann-Whitney test (does not assume ~N())
+# Function to perform t-test
+t_test <- function(field) {
+  mean_heavy <- buying_behaviour %>% 
+                filter(cust_type == "Heavy") %>% 
+                select_(field) %>% sapply(as.numeric)
+  
+  mean_light <- buying_behaviour %>% 
+                filter(cust_type == "Light") %>% 
+                select_(field) %>% sapply(as.numeric)
+  
+  t_test <- t.test(mean_heavy, mean_light, alternative = "two.sided", 
+                   var.equal = FALSE, conf.level = 0.95) %>% tidy()
+  
+  return(t_test)
+}
 
-# AVERAGE WEEKLY SPEND ------------------------------------------
-var_spend_heavy <- var(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_spend)
-var_spend_light <- var(buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_spend)
+# Function to perform Man-Whitney test
+wicox <- function(field) {
+  wilcox.test(eval(parse(text = field)) ~ cust_type, data = buying_behaviour) %>% 
+    tidy()
+}
 
-var_test_spend <- var.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_spend, 
-                           buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_spend, 
-                           alternative = "two.sided")
+# Create table of variances for each field
+variances <- data_frame(var_light = c(get_var("avg_weekly_spend", "Light"),
+                                      get_var("avg_weekly_packs", "Light"),
+                                      get_var("avg_weekly_visits", "Light"),
+                                      get_var("store_loyalty", "Light"),
+                                      get_var("brand_loyalty", "Light"),
+                                      get_var("max_spend", "Light"),
+                                      get_var("min_spend", "Light"),
+                                      get_var("prop_promo_price", "Light"),
+                                      get_var("prop_promo_units", "Light")),
+                        
+                        var_heavy = c(get_var("avg_weekly_spend", "Heavy"),
+                                      get_var("avg_weekly_packs", "Heavy"),
+                                      get_var("avg_weekly_visits", "Heavy"),
+                                      get_var("store_loyalty", "Heavy"),
+                                      get_var("brand_loyalty", "Heavy"),
+                                      get_var("max_spend", "Heavy"),
+                                      get_var("min_spend", "Heavy"),
+                                      get_var("prop_promo_price", "Heavy"),
+                                      get_var("prop_promo_units", "Heavy")))
 
-tTest_spend <- t.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_spend, 
-                      buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_spend, 
-                      alternative = "two.sided", var.equal = FALSE, conf.level = 0.95)
+# Step 1 - Compute statistics for each field -----------------------------------
+# Avg weekly spend
+var_test_spend <- var_test("avg_weekly_spend")
+tTest_spend <- t_test("avg_weekly_spend")
+wilcox_spend <- wicox("avg_weekly_spend")
 
-wilcox_spend <- wilcox.test(avg_weekly_spend ~ cust_type, data = buying_behaviour)
+# Avg. weekly packs
+var_test_packs <- var_test("avg_weekly_packs")
+tTest_packs <- t_test("avg_weekly_packs")
+wilcox_packs <- wicox("avg_weekly_packs")
 
-# ------------------------------------------AVERAGE WEEKLY VISITS --------------
-var_visits_heavy <- var(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_visits)
-var_visits_light <- var(buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_visits)
+# Avg. weekly visits
+var_test_visits <- var_test("avg_weekly_visits")
+tTest_visits <- t_test("avg_weekly_visits")
+wilcox_visits <- wicox("avg_weekly_visits")
 
-var_test_visits <- var.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_visits, 
-                            buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_visits, 
-                            alternative = "two.sided")
+# Store loyalty
+var_test_store_loy <- var_test("store_loyalty")
+tTest_store_loy <- t_test("store_loyalty")
+wilcox_store_loy <- wicox("store_loyalty")
 
-tTest_visits <- t.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_visits, 
-                       buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_visits, 
-                       alternative = "two.sided", var.equal = FALSE, conf.level = 0.95)
+# Brand loyalty
+var_test_brand_loy <- var_test("brand_loyalty")
+tTest_brand_loy <- t_test("brand_loyalty")
+wilcox_brand_loy <- wicox("brand_loyalty")
 
-wilcox_visits <- wilcox.test(avg_weekly_visits ~ cust_type, data = buying_behaviour)
+# Max spend
+var_test_max <- var_test("max_spend")
+tTest_max <- t_test("max_spend")
+wilcox_max <- wicox("max_spend")
 
-# ------------------------------------------AVERAGE WEEKLY PACKS ---------------
-var_packs_heavy <- var(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_packs)
-var_packs_light <- var(buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_packs)
+# min spend
+var_test_min <- var_test("min_spend")
+tTest_min <- t_test("min_spend")
+wilcox_min <- wicox("min_spend")
 
-var_test_packs <- var.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_packs, 
-                           buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_packs, 
-                           alternative = "two.sided")
+# Promo price
+var_test_promo_price <- var_test("prop_promo_price")
+tTest_promo_price <- t_test("prop_promo_price")
+wilcox_promo_price <- wicox("prop_promo_price")
 
-tTest_packs <- t.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_packs, 
-                      buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_packs, 
-                      alternative = "two.sided", var.equal = FALSE, conf.level = 0.95)
+# Promo units
+var_test_promo_units <- var_test("prop_promo_units")
+tTest_promo_units <- t_test("prop_promo_units")
+wilcox_promo_units <- wicox("prop_promo_units")
 
-wilcox_packs <- wilcox.test(avg_weekly_packs ~ cust_type, data = buying_behaviour)
-
-# ------------------------------------------AVERAGE WEEKLY VOLUME --------------
-var_vol_heavy <- var(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_vol)
-var_vol_light <- var(buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_vol)
-
-var_test_vol <- var.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_vol, 
-                         buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_vol, 
-                         alternative = "two.sided")
-
-tTest_vol <- t.test(buying_behaviour[buying_behaviour$cust_type == "Heavy", ]$avg_weekly_vol,
-                    buying_behaviour[buying_behaviour$cust_type == "Light", ]$avg_weekly_vol, 
-                    alternative = "two.sided", var.equal = FALSE, conf.level = 0.95)
-
-wilcox_vol <- wilcox.test(avg_weekly_vol ~ cust_type, data = buying_behaviour)
 
 # Step 3 - tidy up results -----------------------------------------------------
-# Tidy up test results and bind into dataframes --------------------------------
-var_tests    <- rbind(tidy(var_test_spend), tidy(var_test_visits), tidy(var_test_packs), tidy(var_test_vol))
-t_tests      <- rbind(tidy(tTest_spend), tidy(tTest_visits), tidy(tTest_packs), tidy(tTest_vol))
-wilcox_tests <- rbind(tidy(wilcox_spend), tidy(wilcox_visits), tidy(wilcox_packs), tidy(wilcox_vol))
+var_tests    <- bind_rows(var_test_spend, var_test_packs, var_test_visits,
+                          var_test_store_loy, 
+                          var_test_brand_loy, var_test_max, var_test_min,
+                          var_test_promo_price, var_test_promo_units)
 
-# Add variances to var_tests ---------------------------------------------------
-variances <- data.frame(var_spend_light, var_spend_heavy)
-variances <- rbind(variances, c(var_visits_light, var_visits_heavy))
-variances <- rbind(variances, c(var_packs_light, var_packs_heavy))
-variances <- rbind(variances, c(var_vol_light, var_vol_heavy))
+t_tests <- bind_rows(tTest_spend, tTest_packs, tTest_visits, tTest_store_loy, 
+                      tTest_brand_loy, tTest_max, tTest_min,
+                      tTest_promo_price, tTest_promo_units)
 
-var_tests <- cbind(var_tests, variances)
 
-# Rename columns meaningfully and add ID (=variable) ---------------------------
-wilcox_tests       <- rename(wilcox_tests, mann.whitney.u.statistic = statistic)
-t_tests            <- rename(t_tests, difference = estimate, mean_heavy = estimate1, 
-                             mean_light = estimate2, df = parameter)
-var_tests          <- rename(var_tests, f.statistic = statistic, var_light = var_spend_light, 
-                             var_heavy = var_spend_heavy)
-var_tests$estimate <- NULL
+wilcox_tests <- bind_rows(wilcox_spend, wilcox_packs, wilcox_visits,
+                          wilcox_store_loy, 
+                          wilcox_brand_loy, wilcox_max, wilcox_min,
+                          wilcox_promo_price, wilcox_promo_units)
 
+
+
+# Rename columns 
+wilcox_tests <- wilcox_tests %>% rename(mann.whitney.u.statistic = statistic)
+
+t_tests <- t_tests %>% rename(difference = estimate, 
+                              mean_heavy = estimate1, 
+                              mean_light = estimate2, df = parameter)
+
+
+var_tests <- var_tests %>% rename(f.statistic = statistic) %>% 
+                            select(-estimate) %>% 
+                            bind_cols(variances)
+
+
+# Create field identifiers
 variables    <- c("Average spend per visit", 
+                  "Average packs per visit",
                   "Average weekly visits", 
-                  "Average packs per visit", 
-                  "Average volume per visit")
+                  "Store loyalty",
+                  "Brand loyalty",
+                  "Max spend",
+                  "Min spend",
+                  "Prop. price promotion",
+                  "Prop. unit promotion")
+
+
 wilcox_tests <- cbind(wilcox_tests, variables)
 var_tests    <- cbind(var_tests, variables)
 t_tests      <- cbind(t_tests, variables)
